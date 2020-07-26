973cbf54f894b68b1b98dbdc5df17863315e20ea
rexcloud1 <68808736+rexcloud1@users.noreply.github.com>
2020-07-26 10:50:13 +0000

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"Hello welcome to this free Group"
+        f"Hello welcome to this Group")
         quote=True
     )
 
